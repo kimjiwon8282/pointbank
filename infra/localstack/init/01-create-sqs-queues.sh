c25616144c360d499b2932b1368c24157fc8c664
kimjiwon8282 <iwill324@pusan.ac.kr>
2026-07-16 12:07:23 +0900

    fix: make localstack sqs init script portable

--- infra/localstack/init/01-create-sqs-queues.sh
+++ infra/localstack/init/01-create-sqs-queues.sh
@@ -1,34 +1,45 @@
 #!/bin/sh
-set -e
+set -eu
 
-LEDGER_COMMAND_DLQ_URL=$(awslocal sqs create-queue \
-  --queue-name ledger-command-dlq.fifo \
-  --attributes FifoQueue=true,VisibilityTimeout=60,ContentBasedDeduplication=false \
-  --query 'QueueUrl' \
-  --output text)
+REGION="${AWS_DEFAULT_REGION:-ap-northeast-2}"
 
-SECURITIES_RESULT_DLQ_URL=$(awslocal sqs create-queue \
-  --queue-name securities-result-dlq.fifo \
-  --attributes FifoQueue=true,VisibilityTimeout=60,ContentBasedDeduplication=false \
-  --query 'QueueUrl' \
-  --output text)
+create_dlq() {
+  awslocal sqs create-queue \
+    --region "$REGION" \
+    --queue-name "$1" \
+    --attributes FifoQueue=true,VisibilityTimeout=60,ContentBasedDeduplication=false \
+    --query 'QueueUrl' \
+    --output text
+}
 
-LEDGER_COMMAND_DLQ_ARN=$(awslocal sqs get-queue-attributes \
-  --queue-url "$LEDGER_COMMAND_DLQ_URL" \
-  --attribute-names QueueArn \
-  --query 'Attributes.QueueArn' \
-  --output text)
+get_queue_arn() {
+  awslocal sqs get-queue-attributes \
+    --region "$REGION" \
+    --queue-url "$1" \
+    --attribute-names QueueArn \
+    --query 'Attributes.QueueArn' \
+    --output text
+}
 
-SECURITIES_RESULT_DLQ_ARN=$(awslocal sqs get-queue-attributes \
-  --queue-url "$SECURITIES_RESULT_DLQ_URL" \
-  --attribute-names QueueArn \
-  --query 'Attributes.QueueArn' \
-  --output text)
+create_source_queue() {
+  ATTRIBUTES=$(printf '%s' "{\"FifoQueue\":\"true\",\"VisibilityTimeout\":\"60\",\"ContentBasedDeduplication\":\"false\",\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$2\\\",\\\"maxReceiveCount\\\":\\\"5\\\"}\"}")
+  awslocal sqs create-queue \
+    --region "$REGION" \
+    --queue-name "$1" \
+    --attributes "$ATTRIBUTES"
+}
 
-awslocal sqs create-queue \
-  --queue-name ledger-command-queue.fifo \
-  --attributes "FifoQueue=true,VisibilityTimeout=60,ContentBasedDeduplication=false,RedrivePolicy={\"deadLetterTargetArn\":\"$LEDGER_COMMAND_DLQ_ARN\",\"maxReceiveCount\":\"5\"}"
+LEDGER_COMMAND_DLQ_URL=$(create_dlq ledger-command-dlq.fifo)
+SECURITIES_RESULT_DLQ_URL=$(create_dlq securities-result-dlq.fifo)
+LEDGER_ORDER_COMMAND_DLQ_URL=$(create_dlq ledger-order-command-dlq.fifo)
+SECURITIES_ORDER_RESULT_DLQ_URL=$(create_dlq securities-order-result-dlq.fifo)
 
-awslocal sqs create-queue \
-  --queue-name securities-result-queue.fifo \
-  --attributes "FifoQueue=true,VisibilityTimeout=60,ContentBasedDeduplication=false,RedrivePolicy={\"deadLetterTargetArn\":\"$SECURITIES_RESULT_DLQ_ARN\",\"maxReceiveCount\":\"5\"}"
+LEDGER_COMMAND_DLQ_ARN=$(get_queue_arn "$LEDGER_COMMAND_DLQ_URL")
+SECURITIES_RESULT_DLQ_ARN=$(get_queue_arn "$SECURITIES_RESULT_DLQ_URL")
+LEDGER_ORDER_COMMAND_DLQ_ARN=$(get_queue_arn "$LEDGER_ORDER_COMMAND_DLQ_URL")
+SECURITIES_ORDER_RESULT_DLQ_ARN=$(get_queue_arn "$SECURITIES_ORDER_RESULT_DLQ_URL")
+
+create_source_queue ledger-command-queue.fifo "$LEDGER_COMMAND_DLQ_ARN"
+create_source_queue securities-result-queue.fifo "$SECURITIES_RESULT_DLQ_ARN"
+create_source_queue ledger-order-command-queue.fifo "$LEDGER_ORDER_COMMAND_DLQ_ARN"
+create_source_queue securities-order-result-queue.fifo "$SECURITIES_ORDER_RESULT_DLQ_ARN"
